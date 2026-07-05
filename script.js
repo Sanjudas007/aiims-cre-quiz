14e26c96c60061faf9021a31b59ec832f8db362e
Sanjudas007 <75924899+Sanjudas007@users.noreply.github.com>
2026-07-06 00:15:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,81 @@
-function startExam(){
+const questions=[
+{
+question:"Which rule governs Joining Time?",
+options:[
+"CCS Leave Rules",
+"FR & SR",
+"GFR",
+"Conduct Rules"
+],
+answer:1
+},
+{
+question:"Which is a major penalty?",
+options:[
+"Censure",
+"Dismissal",
+"Warning",
+"Advice"
+],
+answer:1
+}
+];
 
-let name=document.getElementById("name").value;
+let current=0;
 
-if(name==""){
+function loadQuestion(){
 
-alert("Enter Candidate Name");
+document.getElementById("qNo").innerHTML=
+"Question "+(current+1)+" of "+questions.length;
 
-return;
+document.getElementById("question").innerHTML=
+questions[current].question;
+
+document.getElementById("o1").innerHTML=
+questions[current].options[0];
+
+document.getElementById("o2").innerHTML=
+questions[current].options[1];
+
+document.getElementById("o3").innerHTML=
+questions[current].options[2];
+
+document.getElementById("o4").innerHTML=
+questions[current].options[3];
 
 }
 
-window.location="exam.html";
+if(document.getElementById("question")){
+
+loadQuestion();
+
+}
+
+let time=5400;
+
+setInterval(function(){
+
+let m=Math.floor(time/60);
+
+let s=time%60;
+
+document.getElementById("timer").innerHTML=
+String(m).padStart(2,"0")+":"+
+String(s).padStart(2,"0");
+
+time--;
+
+},1000);
+
+let p=document.getElementById("palette");
+
+if(p){
+
+for(let i=1;i<=100;i++){
+
+p.innerHTML+=
+"<button>"+i+"</button>";
+
+}
 
 }
